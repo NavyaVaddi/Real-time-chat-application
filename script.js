3df0b33cdf7093ecd8abc516fdcfb51bd2a8fe03
NavyaVaddi <vaddinavya.g@gmail.com>
2023-07-27 17:05:35 +0530

    update file.html

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// Function to handle sending a message
+function sendMessage(){
+ const messageInput = document.getElementById('message-input');
+ const message = messageInput.ariaValueMax.trim();
+ if(message !== '') {
+  const chatMessages = document.querySelector('.chat-messages');
+  const messageElement = document.createElement('div');
+  messageElement.classList.add('message');
+  messageElement.textContent = message;
+  charMessages.appendChild(messageElement);
+  messageInput.value = '';
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+ }
+}
+
+// Event listener
+document.getElementById('send-button').addEventListener('click', sendMessage);
+document.getElementById('message-input').addEventListener('keydown', (event) => {
+
+ if (event.key === 'Enter'){
+  event.preventDefault();
+  sendMessage();
+ }
+});
